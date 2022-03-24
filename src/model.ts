a36b595f225e77ae89cf9d34922ec14926226b64
Jihee Kim <jiheekim93@gmail.com>
2022-03-24 12:56:52 -0400

    wow

---
+++ src/model.ts
@@ -0,0 +1,9 @@
+export interface Planet{
+    id: number;
+    name: string;
+    image: string;
+    description: string;
+    date_found: string;
+    ticket_price: string;
+    activity: string;
+}
